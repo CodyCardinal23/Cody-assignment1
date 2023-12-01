6d85fd595f8142c2d669b15178fee93660e0c905
CodyCardinal23 <codycardinal@gmail.com>
2023-11-30 21:29:19 -0700

    V2 Changes

--- src/com/coderscampus/Assignment1Application.java
+++ src/com/coderscampus/Assignment1Application.java
@@ -4,7 +4,12 @@ public class Assignment1Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+ 
+		// generate a random number
+		
+		// and collect user input 
+		
+		// etc.
 	}
 
 }
